d07625dee30e70ddea0a83bae65c5096f163f0fe
apache17 <brgp@live.com>
2018-10-15 16:21:32 -0600

    iniciando repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Lista de proyectos de GitHub!")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
